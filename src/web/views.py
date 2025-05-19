23591c8f951b1351d4c96bdbb6b302d3b2323847
arcstur <arcstur@gmail.com>
2025-05-19 18:38:16 -0300

    squash me

--- src/web/views.py
+++ src/web/views.py
@@ -4,4 +4,4 @@ from django.views.decorators.http import require_safe
 
 @require_safe
 def render_react_index(request):
-    return render(request, "frontend/index.html", {})
+    return render(request, "index.html", {})
