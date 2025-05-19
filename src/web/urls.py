b2b6a76ab11c79947c8e018eb9d7c5cec65eab06
arcstur <arcstur@gmail.com>
2025-05-19 18:35:54 -0300

    feat: django-admin startapp web for frontend stuff

---
+++ src/web/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import render_react_index
+
+urlpatterns = [
+    path("", render_react_index),
+    path("infographics", render_react_index),
+    path("infographics/", render_react_index),
+]
