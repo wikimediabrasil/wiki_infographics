0a750d3a3454dd330c5310b21ef23df6a614f820
arcstur <arcstur@gmail.com>
2025-07-25 17:59:54 -0300

    refactor: complete refactor of bar chart race, still no categories

--- src/graphs/tests.py
+++ src/graphs/tests.py
@@ -1,9 +1,12 @@
 import pandas as pd
+from numpy import nan
 from django.test import TestCase
 from django.utils.timezone import now
 
 from graphs.table import process_table
 from graphs.bar_chart_race import process_bar_chart_race
+from graphs.bar_chart_race import BaseDf
+from graphs.bar_chart_race import DfProcessor
 
 
 class TestHelper:
@@ -18,6 +21,31 @@ class TestHelper:
                 {
                     "item": "http://www.wikidata.org/entity/Q6669324",
                     "itemLabel": "Lomba do Pinheiro",
+                },
+            ]
+        )
+
+    @staticmethod
+    def mock_df_bcr():
+        return pd.DataFrame(
+            [
+                {
+                    "item": "http://www.wikidata.org/entity/Q40269",
+                    "itemLabel": "Porto Alegre",
+                    "population": 1332845,
+                    "date": "2022-01-01T00:00:00Z",
+                },
+                {
+                    "item": "http://www.wikidata.org/entity/Q43463",
+                    "itemLabel": "Fortaleza",
+                    "population": 2428708,
+                    "date": "2022-01-01T00:00:00Z",
+                },
+                {
+                    "item": "http://www.wikidata.org/entity/Q174",
+                    "itemLabel": "São Paulo",
+                    "population": 12325232,
+                    "date": "2020-07-01T00:00:00Z",
                 },
             ]
         )
@@ -45,14 +73,88 @@ class QueryTests(TestCase):
     def test_bcr_base_errors(self):
         df = TestHelper.mock_df_table()
         msg = process_bar_chart_race(df)["failed"]
-        self.assertEqual(msg, "number of columns must be between 3 and 6")
+        self.assertEqual(msg, "number of columns must be between 3 and 5")
+
+        df = TestHelper.mock_df_table()
+        df["A"] = "A"
+        df["B"] = 1234
+        df["C"] = now()
+        df["D"] = "D"
+        msg = process_bar_chart_race(df)["failed"]
+        self.assertEqual(msg, "number of columns must be between 3 and 5")
+
+        df = TestHelper.mock_df_table()
         df["new column"] = "abcdef"
         msg = process_bar_chart_race(df)["failed"]
         self.assertEqual(msg, "last column must be a date column")
+
+        df = TestHelper.mock_df_table()
         df["new column"] = now()
         msg = process_bar_chart_race(df)["failed"]
         self.assertEqual(msg, "second to last column must be a quantity column")
+
+        df = TestHelper.mock_df_table()
         df["new column"] = 2
         df["last column"] = now()
         res = process_bar_chart_race(df)
         self.assertNotIn("failed", res)
+
+        df = TestHelper.mock_df_table()
+        df["new column"] = 2
+        df["last column"] = "2025-01-01"
+        res = process_bar_chart_race(df)
+        self.assertNotIn("failed", res)
+
+        df = TestHelper.mock_df_bcr()
+        df.drop(columns="itemLabel", inplace=True)
+        msg = process_bar_chart_race(df)["failed"]
+        self.assertEqual(msg, "there should be at least one label")
+
+        df = TestHelper.mock_df_bcr()
+        df.drop(columns="item", inplace=True)
+        res = process_bar_chart_race(df)
+        self.assertNotIn("failed", res)
+
+    def test_base_df(self):
+        df = TestHelper.mock_df_bcr()
+        BaseDf(df).prepare()
+        self.assertEqual(set(df.columns), set(("name", "url", "value", "date")))
+
+    def test_df_processor(self):
+        df = TestHelper.mock_df_bcr()
+        bdf = BaseDf(df).prepare()
+        proc = DfProcessor(bdf)
+        elements = proc.elements()
+        self.assertEqual(
+            elements,
+            {
+                "Porto Alegre": {"url": "http://www.wikidata.org/entity/Q40269"},
+                "Fortaleza": {"url": "http://www.wikidata.org/entity/Q43463"},
+                "São Paulo": {"url": "http://www.wikidata.org/entity/Q174"},
+            },
+        )
+        ip = proc.interpolated_df()
+        self.assertEqual(
+            list(ip[ip["name"] == "São Paulo"]["value"]),
+            [
+                12325232.0,
+                12325232.0,
+                12325232.0,
+            ],
+        )
+        self.assertEqual(
+            list(ip[ip["name"] == "Porto Alegre"]["value"].fillna(0)),
+            [
+                0,
+                0,
+                1332845.0,
+            ],
+        )
+        self.assertEqual(
+            list(ip[ip["name"] == "Fortaleza"]["value"].fillna(0)),
+            [
+                0,
+                0,
+                2428708.0,
+            ],
+        )
