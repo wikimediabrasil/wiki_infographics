0aa860d6a9a029671c656318f7b0c252be09c4aa
arcstur <arcstur@gmail.com>
2025-08-07 13:38:03 -0300

    refactor: add repository to navbar

--- client/src/Components/NavBar/navBar.jsx
+++ client/src/Components/NavBar/navBar.jsx
@@ -25,8 +25,7 @@ function NavBar({username}) {
 
         <div className="block py-2 pl-3 pr-4 md:p-0 border-b border-gray-100 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-cyan-700 md:dark:hover:bg-transparent md:dark:hover:text-white">
           <Dropdown label="Help" inline>
-            <Dropdown.Item>Help Portal</Dropdown.Item>
-            <Dropdown.Item>User Manual</Dropdown.Item>
+            <Dropdown.Item href="https://github.com/wikimediabrasil/wiki_infographics">Repository</Dropdown.Item>
           </Dropdown>
         </div>
 
