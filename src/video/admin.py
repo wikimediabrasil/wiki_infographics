f01fd1a217133a3a3bb0b91b70f5b370157538f6
arcstur <arcstur@gmail.com>
2025-08-11 18:11:04 -0300

    feat: video admin

--- src/video/admin.py
+++ src/video/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from video.models import Video
+from video.models import VideoFrame
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ["id", "created", "modified"]
+
+@admin.register(VideoFrame)
+class VideoFrameAdmin(admin.ModelAdmin):
+    list_display = ["video", "index", "created"]
+    raw_id_fields = ["video"]
