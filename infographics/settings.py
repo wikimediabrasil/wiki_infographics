be0c8ac579db4fba0a06a98ed9a397e27b2a21d1
arcstur <arcstur@gmail.com>
2025-08-14 17:48:56 -0300

    fix: use tool's home when available

--- infographics/settings.py
+++ infographics/settings.py
@@ -92,10 +92,15 @@ WSGI_APPLICATION = 'infographics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+if os.environ.get("TOOL_DATA_DIR"):
+    db_name = Path(os.environ["TOOL_DATA_DIR"]) / "db.sqlite3"
+else:
+    db_name = BASE_DIR / 'db.sqlite3'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': db_name,
     }
 }
 
