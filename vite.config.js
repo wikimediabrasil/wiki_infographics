166b6f9d8fdef3f92f367c59655995e8091df036
arcstur <arcstur@gmail.com>
2025-08-14 18:47:32 -0300

    build: update toolforge deployment and local development

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,18 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  // Load environment variables based on the current mode (`development`, `production`, etc.)
   const env = loadEnv(mode, process.cwd())
+  const base = env.VITE_DEV === "True" ? "" : "/static/frontend";
 
   return {
-    base: '/static/frontend/',
+    base: base,
     plugins: [react()],
     server: {
       proxy: {
+        // the proxy only happens in development, because
+        // django is handling the requests and gets /api to it
         '/api': {
-          target: `${env.VITE_API_BASE_URL}:${env.VITE_API_PORT}`,
+          target: "http://127.0.0.1:8000",
           changeOrigin: true,
           secure: false,
         },
