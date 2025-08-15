50151f30a028da51d83ea868341dc7e0f902c43f
arcstur <arcstur@gmail.com>
2025-08-15 14:03:02 -0300

    fix: create directories in compile_frontend.sh

--- compile_frontend.sh
+++ compile_frontend.sh
@@ -1,7 +1,9 @@
 set -xeu
 vite build
 echo "Copying to Django..."
+mkdir -p ./web/templates
 mv ./dist/index.html ./web/templates/index.html
+mkdir -p ./web/static/frontend
 rm -rf ./web/static/frontend/assets
 mv ./dist/assets ./web/static/frontend/assets
 rmdir ./dist
