a84cae6f3238c62da472e329486f7954f915e825
arcstur <arcstur@gmail.com>
2025-09-03 15:35:24 -0300

    feat: import all translations automatically

--- src/context/LanguageContext.jsx
+++ src/context/LanguageContext.jsx
@@ -1,33 +1,39 @@
 import { createContext, useState, useEffect } from "react";
 import languageToLocaleMap from '../utils/locale.js';
-import content_en from "../../translations/en.json";
-import content_pt_br from "../../translations/pt-br.json";
+const translations = import.meta.glob(["../../translations/*.json", "!../../translations/qqq.json"], { import: "default", eager: true });
 
 const LanguageContext = createContext();
 
+function getContents() {
+  var contents = {};
+  for (const path in translations) {
+    const languageCodeWithJson = path.split("/").slice(-1)[0];
+    const languageCode = languageCodeWithJson.substring(0, languageCodeWithJson.length - 5);
+    contents[languageCode] = translations[path];
+  };
+  return contents
+}
+
 function LanguageProvider(props) {
   const DEFAULT_LANGUAGE = "en";
-  const CONTENTS = {
-    "en": content_en,
-    "pt-br": content_pt_br,
-  }
-  const availableLanguages = Object.keys(CONTENTS);
+
+  const contents = getContents();
+  const availableLanguages = Object.keys(contents);
 
   const [language, setLanguage] = useState(() => localStorage.language || DEFAULT_LANGUAGE);
-  const [translatedContent, setTranslatedContent] = useState(CONTENTS[language]);
+  const [translatedContent, setTranslatedContent] = useState(contents[language] || contents[DEFAULT_LANGUAGE]);
   const [locale, setLocale] = useState(undefined);
 
   useEffect(() => {
     if (language) {
       localStorage.language = language;
-      setTranslatedContent(CONTENTS[language]);
-      // undefined uses the browser's default locale
-      setLocale(languageToLocaleMap[language] || undefined);
+      setTranslatedContent(contents[language] || contents[DEFAULT_LANGUAGE]);
+      setLocale(languageToLocaleMap[language] || undefined); // undefined uses the browser's default locale
     };
   }, [language]);
 
   const getContent = (key) => {
-    return translatedContent[key] || CONTENTS[DEFAULT_LANGUAGE][key];
+    return translatedContent[key] || contents[DEFAULT_LANGUAGE][key];
   };
 
   return (
